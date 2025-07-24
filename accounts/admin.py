9a3160cde409d9ab7047e2810e8be2acb5771722
kelson <kelsontrd@gmail.com>
2025-07-24 17:48:31 -0300

    initical commit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from accounts.models.account.Account import Account
+from accounts.models.account.AccountType import AccountType
 
-# Register your models here.
+admin.site.register(Account)
+admin.site.register(AccountType)
